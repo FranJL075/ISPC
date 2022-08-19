f4ecef6eccd5b17a6696326c4195c7d0d424b756
Francisco Junco Lasagno <70384142+FranJL075@users.noreply.github.com>
2022-08-19 19:27:18 -0300

    Add files via upload

---
+++ 1.Introduccion a la Programacion.py
@@ -0,0 +1,8 @@
+#Estructura: Dueno(nombre de la lista) = [DNI(int), nombre(string), apellido(string), teléfono(int), dirección(string)]
+#Dueno datos:
+Dueno = [28957346, "Juan", "Perez", 4789689, "Belgrano 101"]
+
+if Dueno[0] > 26000000:
+    print(Dueno[0])
+
+
